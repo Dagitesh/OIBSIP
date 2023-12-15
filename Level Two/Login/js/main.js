b9b8fac8df367af2c4d279a28e157229f1099cb3
Your Name <you@example.com>
2023-12-15 21:40:22 +0300

    login page done

--- Level Two/Login/js/main.js
+++ Level Two/Login/js/main.js
@@ -1,26 +1,16 @@
-/*=============== SHOW HIDDEN - PASSWORD ===============*/
-const showHiddenPass = (loginPass, loginEye) =>{
-    const input = document.getElementById(loginPass),
-          iconEye = document.getElementById(loginEye)
- 
-    iconEye.addEventListener('click', () =>{
-       // Change password to text
-       if(input.type === 'password'){
-          // Switch to text
-          input.type = 'text'
- 
-          // Icon change
-          iconEye.classList.add('ri-eye-line')
-          iconEye.classList.remove('ri-eye-off-line')
-       } else{
-          // Change to password
-          input.type = 'password'
- 
-          // Icon change
-          iconEye.classList.remove('ri-eye-line')
-          iconEye.classList.add('ri-eye-off-line')
-       }
-    })
- }
- 
- showHiddenPass('login-pass','login-eye')
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // Get the input values
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    
+    // Perform authentication
+    if (username === 'admin' && password === 'password') {
+      // Authentication successful
+      window.location.href = 'welcome.html';
+    } else {
+    
+      alert('Invalid username or password. Please try again.');
+    }
+  });
